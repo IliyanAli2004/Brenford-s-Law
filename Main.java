c38ebd17d84c0b2539f2cee651275893da1821ec
AmyyyyyLiu <81171292+AmyyyyyLiu@users.noreply.github.com>
2021-05-12 09:52:38 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -14,7 +14,7 @@ import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
 import javafx.stage.Stage;
 
-public class Main { extends Application 
+public class Main extends Application {
 
     public static void main(String[] args) throws FileNotFoundException {
         
